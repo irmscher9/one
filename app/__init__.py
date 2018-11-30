7f1eee904ffcf05434faf5f30de2fde36a07b610
irmscher9 <irmscher9@users.noreply.github.com>
2018-11-30 18:20:58 +0200

    latest

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = '/database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = b'x1cVx#cx8akxc8#05xa7x97#afVxf2skx91x0e'
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from app import routes
